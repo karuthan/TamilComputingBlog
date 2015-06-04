722d5a252a2ef3078ee6ed97f472a8e76008e2f9
karuthan <kanasinapana@gmail.com>
2015-06-04 23:48:53 +0100

    test angular

---
+++ js/vendor/testApp.js
@@ -0,0 +1,28 @@
+( function() {
+  'use strict';  
+  angular.module( 'testApp.routing', ['ui.router'] );
+  angular.module( 'testApp.controllers', [] );
+  angular.module( 'testApp.directives', [] );
+  angular.module( 'testApp.filters', [] );
+  angular.module( 'testApp.services', [] );
+  angular.module( 'testApp.constants', [] );
+  angular.module( 'testApp', ['testApp.controllers'] );
+  var myApp = angular.module('testApp');
+  myApp.config([
+  '$interpolateProvider', function($interpolateProvider) {
+    return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+  }
+  ]);
+} )();
+
+
+( function() {
+
+  'use strict';
+  angular.module( 'testApp.controllers' ).controller( 'testController', 
+  function( $scope ) {
+	
+    $scope.results = ['Killer','Ilan','Prince']; 
+	
+	})
+})();
